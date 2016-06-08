f6ac5b6dc3f0792936ed2974944021457b647b44
Elina <eli.lisenochka@gmail.com>
2016-06-08 17:25:26 +0300

    triangle and board

---
+++ triangle.js
@@ -0,0 +1,19 @@
+
+
+/*дз */
+
+function hash(){
+var str="#";
+	for(var i=1;i<=10;++i){
+	document.write(str+"<br>");
+	str=str+"#";
+}
+}
+
+
+function board(){
+var str="#########";
+	for(var i=1;i<=8;++i){
+	document.write(str+"<br>");
+}
+}
